7aebb91dfdab9cf7cdfcaec095c04c94408b6d10
Pranav K <pranavkovvammal@croswer.com>
2023-04-21 15:04:39 +0530

    label fix

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Restaurent Admin',
+      title: 'Restaurent Owner',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
